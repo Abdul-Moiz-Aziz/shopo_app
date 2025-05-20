e92f49ef75307357f3beb0c571e66dc8cae5aba9
AMASourceCodes <qureshimoiz1111@gmail.com>
2025-05-21 03:33:40 +0500

    initial commit

---
+++ backend/config/config.js
@@ -0,0 +1,13 @@
+const config = {
+      development: {
+            db: 'mongodb://localhost/dev-db',
+            port: 3000
+      },
+      production: {
+            db: 'mongodb://prod-db-url',
+            port: process.env.PORT || 80
+      }
+};
+
+module.exports = config[process.env.NODE_ENV || 'development'];
+    
---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className='text-[20px]'>hello world</div>
+  )
+}
+
+export default App
