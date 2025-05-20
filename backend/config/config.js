84899861a873913e300be3ed21c471a2b25f3ee4
AMASourceCodes <qureshimoiz1111@gmail.com>
2025-05-21 03:46:07 +0500

    update cloudinary.js

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,13 +1,13 @@
-const config = {
-      development: {
-            db: 'mongodb://localhost/dev-db',
-            port: 3000
-      },
-      production: {
-            db: 'mongodb://prod-db-url',
-            port: process.env.PORT || 80
-      }
-};
+// const config = {
+//       development: {
+//             db: 'mongodb://localhost/dev-db',
+//             port: 3000
+//       },
+//       production: {
+//             db: 'mongodb://prod-db-url',
+//             port: process.env.PORT || 80
+//       }
+// };
 
-module.exports = config[process.env.NODE_ENV || 'development'];
+// module.exports = config[process.env.NODE_ENV || 'development'];
     
