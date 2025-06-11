6606e1e850448f3083e508b954601f31c23fc1e6
AMA Code <161360768+Abdul-Moiz-Aziz@users.noreply.github.com>
2025-06-11 18:55:01 +0000

    update files

--- frontend/src/components/common/footer.jsx
+++ frontend/src/components/common/footer.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
 import MainFooter from "./mainFooter";
-import FooterBottom from "./FooterBottom";
+import FooterBottom from "./footerBottom";
 
 const Footer = () => {
   return (
